6bc5b43828cd7658f432c4233a648a0bb7d5be6d
samchencode <samchencode@gmail.com>
2021-07-09 06:15:38 -0400

    fix: dont use react-native-paper elements

--- src/features/home-screen/IntakeChart.jsx
+++ src/features/home-screen/IntakeChart.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Card, Text } from 'react-native-paper';
+import { View } from 'react-native';
 import Svg, { Rect } from 'react-native-svg';
+import { useTheme } from 'react-native-paper';
 
 function IntakeChart() {
+
+  const { colors } = useTheme();
+
   return(
-    <Card style={{ margin: 10}}>
+    <View style={{
+      margin: 10,
+      width: 200,
+      backgroundColor: colors.light,
+      shadowColor: colors.dark,
+      shadowOpacity: 0.25,
+      shadowRadius: 5,
+      elevation: 5,
+    }}>
       <Svg width={100} height={100} viewBox='0 0 100 100'>
         <Rect x={0} y={0} height={100} width={100} fill="black" />
       </Svg>
-      <Card.Title title="Hi"/>
-    </Card>
+    </View>
   )
 }
 
