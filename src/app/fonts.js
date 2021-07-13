97b6cc6098b34a003de72c0139f57a90dac69162
samchencode <samchencode@gmail.com>
2021-07-12 23:19:38 -0400

    fix: only load fonts we use

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -2,35 +2,17 @@ import { useState, useEffect } from 'react';
 import * as Font from 'expo-font';
 import {
   useFonts,
-  Roboto_100Thin,
-  Roboto_100Thin_Italic,
   Roboto_300Light,
-  Roboto_300Light_Italic,
   Roboto_400Regular,
-  Roboto_400Regular_Italic,
-  Roboto_500Medium,
-  Roboto_500Medium_Italic,
   Roboto_700Bold,
-  Roboto_700Bold_Italic,
-  Roboto_900Black,
-  Roboto_900Black_Italic,
 } from '@expo-google-fonts/roboto';
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function useAppFonts() {
   const [fontsLoaded] = useFonts({
-    Roboto_100Thin,
-    Roboto_100Thin_Italic,
     Roboto_300Light,
-    Roboto_300Light_Italic,
     Roboto_400Regular,
-    Roboto_400Regular_Italic,
-    Roboto_500Medium,
-    Roboto_500Medium_Italic,
     Roboto_700Bold,
-    Roboto_700Bold_Italic,
-    Roboto_900Black,
-    Roboto_900Black_Italic,
   });
 
   return fontsLoaded;
@@ -38,7 +20,7 @@ function useAppFonts() {
 
 function useAppIcons() {
   const [ready, setReady] = useState(false);
-  const fonts = [Ionicons.font, MaterialCommunityIcons.font];
+  const fonts = [MaterialCommunityIcons.font];
 
   useEffect(() => {
     const loadFonts = fonts.map((f) => Font.loadAsync(f));
