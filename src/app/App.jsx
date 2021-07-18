58d91078b7dd226bff006631d9339f4883f2a1be
samchencode <samchencode@gmail.com>
2021-07-18 06:03:49 -0400

    style: remove unused import

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,19 +1,10 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useAppIcons, useAppFonts } from './fonts';
 import Router from './Router';
-import Timer from '../features/timer';
 
 function App() {
-  useEffect(() => {
-    const timer = new Timer();
-    timer.setOnTick(({ dt, timeRemaining }) =>
-    console.log('tick', dt, 'secs', timeRemaining.getSeconds())
-    );
-    timer.setOnEnd(() => console.log('end!'))
-    timer.start({ milliseconds: 60000 });
-  }, []);
 
   const fontsLoaded = useAppFonts();
   const iconsLoaded = useAppIcons();
