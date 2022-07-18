21e27c00bb070a6c86ddfdaa1e60a23f4160ba54
samchencode <samchencode@gmail.com>
2022-07-18 12:36:12 -0400

    test: homescreen

---
+++ __tests__/view/HomeScreen.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HomeScreenFactory from '@/view/home-screen';
+
+describe('<HomeScreen />', () => {
+  const HomeScreen = HomeScreenFactory();
+
+  it('has 1 child', () => {
+    const tree = renderer.create(<HomeScreen />).toJSON();
+
+    // @ts-ignore
+    expect(tree.children.length).toBe(1);
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer.create(<HomeScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
