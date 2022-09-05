097a844719453e910c967f91be0f3d997bbc2b40
samchencode <samchencode@gmail.com>
2022-09-04 20:35:49 -0400

    test: IntakeRecord behavior

--- __tests__/domain/models/IntakeRecord.spec.ts
+++ __tests__/domain/models/IntakeRecord.spec.ts
@@ -11,4 +11,32 @@ describe('IntakeRecord', () => {
       expect(create).not.toThrowError();
     });
   });
+
+  describe('Behavior', () => {
+    it('should get date of void', () => {
+      const datetime1 = new DateAndTime(new Date(0));
+      const urineVolume = new VolumeInMl(8);
+
+      const voidRecord1 = new IntakeRecord(datetime1, urineVolume);
+
+      expect(voidRecord1.getTime()).toBe('12:00 AM');
+      expect(voidRecord1.getDate()).toBe('Jan 1, 1970');
+
+      const datetime2 = new DateAndTime(new Date(1662318939966));
+
+      const voidRecord2 = new IntakeRecord(datetime2, urineVolume);
+
+      expect(voidRecord2.getTime()).toBe('7:15 PM');
+      expect(voidRecord2.getDate()).toBe('Sep 4, 2022');
+    });
+
+    it('should get the volume voided', () => {
+      const datetime = new DateAndTime(new Date(0));
+      const urineVolume = new VolumeInMl(8);
+
+      const voidRecord = new IntakeRecord(datetime, urineVolume);
+
+      expect(voidRecord.getIntakeVolumeInMl()).toBe('8 mL');
+    });
+  });
 });
