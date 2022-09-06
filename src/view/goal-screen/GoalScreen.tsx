144d33b782f914fc00cb706ce9708048c0830f38
samchencode <samchencode@gmail.com>
2022-09-06 08:08:08 -0400

    feat: target svg

--- src/view/goal-screen/GoalScreen.tsx
+++ src/view/goal-screen/GoalScreen.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TargetSvg } from '@/view/goal-screen/TargetSvg';
 
 export function factory() {
   return function GoalScreen() {
+    const svgHeight = 400;
+    const svgWidth = (svgHeight * 2) / 3;
+
     return (
       <View style={styles.container}>
+        <TargetSvg height={svgHeight} width={svgWidth} />
         <Text>Hello from GoalScreen!</Text>
       </View>
     );
