e4e1a515b043be257abd9d5f1384ee7a8a45e95f
samchencode <samchencode@gmail.com>
2022-09-06 10:01:22 -0400

    refactor: deprecated expo apploading

--- src/view/app/App.tsx
+++ src/view/app/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
+import React, { useCallback } from 'react';
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import type { Type as Router } from '@/view/router';
 import { useAppFonts } from '@/view/app/useAppFonts';
+
+SplashScreen.preventAutoHideAsync();
 
 export function factory(Router: Router) {
   return function App() {
     const fontsLoaded = useAppFonts();
-    if (!fontsLoaded) return <AppLoading />;
-    return <Router />;
+    const appIsReady = fontsLoaded;
+
+    const onLayoutRoot = useCallback(
+      async () => appIsReady && (await SplashScreen.hideAsync()),
+      [appIsReady]
+    );
+
+    if (!appIsReady) return null;
+    return (
+      <View onLayout={onLayoutRoot} style={{ flex: 1 }}>
+        <Router />
+      </View>
+    );
   };
 }
 
