28deb99ec2b598aec35511058a8f75aaddb853db
samchencode <samchencode@gmail.com>
2022-09-06 15:56:25 -0400

    feat: map record to recordcard

--- src/view/record-screen/RecordScreen.tsx
+++ src/view/record-screen/RecordScreen.tsx
@@ -7,6 +7,7 @@ import {
   VoidRecordRow,
   RecordCard,
   RecordSectionHeader,
+  ViewRecordVisitor,
 } from '@/view/record-screen/components';
 import type { GetAllRecordsAction } from '@/application/GetAllRecordsAction';
 import type { Record } from '@/domain/models/Record';
@@ -19,34 +20,27 @@ export function factory(getAllRecordsAction: GetAllRecordsAction) {
       getAllRecordsAction.execute().then((v) => setRecords(v));
     }, []);
 
+    const visitor = new ViewRecordVisitor();
+    records.forEach((r) => r.acceptVisitor(visitor));
+    const cards = visitor
+      .getRecordRows()
+      .map(([r, k]) => (
+        <RecordCard recordRow={r} style={styles.card} key={k} />
+      ));
+
     return (
       <View style={styles.container}>
         <StatusBar statusBarStyle="dark" color="transparent" />
         <RecordSectionHeader date="Sept 9th 2022" />
         <RecordCard
-          recordRow={
-            <IntakeRecordRow label="Example" volume="8oz" time="7:00 PM" />
-          }
+          recordRow={<IntakeRecordRow volume="8oz" time="7:00 PM" />}
           style={styles.card}
         />
         <RecordCard
-          recordRow={
-            <VoidRecordRow label="Example" volume="8oz" time="7:15 PM" />
-          }
+          recordRow={<VoidRecordRow volume="8oz" time="7:15 PM" />}
           style={styles.card}
         />
-        {records.length > 0 && (
-          <RecordCard
-            recordRow={
-              <IntakeRecordRow
-                label="Intake"
-                volume="8oz"
-                time={records[0].getTimeString()}
-              />
-            }
-            style={styles.card}
-          />
-        )}
+        {records.length > 0 && cards}
       </View>
     );
   };
