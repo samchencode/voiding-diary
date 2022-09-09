9855d2a2cc1bd6fa4822740f44ab5cb75403365a
samchencode <samchencode@gmail.com>
2022-09-09 08:55:03 -0400

    refactor: versioning database file

--- src/infrastructure/persistence/expo-sqlite/expoSqliteDatabaseFactory.ts
+++ src/infrastructure/persistence/expo-sqlite/expoSqliteDatabaseFactory.ts
@@ -1,5 +1,5 @@
 import { openDatabase } from 'expo-sqlite';
 
-const DATABASE_FILE = 'data.db';
+const DATABASE_FILE = 'data-v1.db';
 
 export const expoSqliteDatabaseFactory = () => openDatabase(DATABASE_FILE);
