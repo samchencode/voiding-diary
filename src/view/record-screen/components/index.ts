85a512791fbcd04a4f2369ac2cc554ddb6603c22
samchencode <samchencode@gmail.com>
2022-10-25 15:03:02 -0400

    style: remove unused file

--- src/view/record-screen/components/index.ts
+++ src/view/record-screen/components/index.ts
@@ -1,4 +1,3 @@
 export { RecordSectionHeader } from '@/view/record-screen/components/RecordSectionHeader';
-export { RecordCard } from '@/view/record-screen/components/RecordCard';
 export { ListHeader } from '@/view/record-screen/components/ListHeader';
 export { ListEmpty } from '@/view/record-screen/components/ListEmpty';
