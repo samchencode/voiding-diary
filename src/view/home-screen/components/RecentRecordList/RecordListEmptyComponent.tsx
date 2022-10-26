dbaefd5421751d43bef79154dee7374531acb6d7
samchencode <samchencode@gmail.com>
2022-10-25 20:43:19 -0400

    fix: empty recent record list spacing

--- src/view/home-screen/components/RecentRecordList/RecordListEmptyComponent.tsx
+++ src/view/home-screen/components/RecentRecordList/RecordListEmptyComponent.tsx
@@ -4,13 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 function RecordListEmptyComponent() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>No records yet for today...</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    marginTop: theme.spaces.sm,
+    marginLeft: theme.spaces.lg,
+    marginRight: theme.spaces.lg,
+  },
   title: {
     ...theme.fonts.sm,
   },
