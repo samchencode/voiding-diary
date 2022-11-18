5dca4dc33d04efe442aed12dbd1160ca85711c6b
samchencode <samchencode@gmail.com>
2022-11-18 05:35:57 -0500

    build: dont mangle names for di

--- metro.config.js
+++ metro.config.js
@@ -6,4 +6,6 @@ config.resolver.assetExts.push('ejs');
 config.resolver.extraNodeModules['fs'] = config.resolver.emptyModulePath;
 config.resolver.extraNodeModules['path'] = config.resolver.emptyModulePath;
 
+config.transformer.minifierConfig.mangle = false;
+
 module.exports = config;
